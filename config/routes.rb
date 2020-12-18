0719e7d23ee04c68e16732421e8a1e6c64f9eae4
Charles Crane <charlie.crane46@gmail.com>
2020-12-18 14:49:38 +0000

    Layout for homepage and login/signup pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/dashboard', to: 'pages#dashboard', as: 'dashboard'
+  get '/search', to: 'books#new', as: 'search'
+
+  resources :books
 end
